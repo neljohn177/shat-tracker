b7af03e2b656db9e4dec862ca74c6eba173f12d4
neljohnmambandingBSIT1B <neljohnmambanling@aclcbukidnon.com>
2025-04-02 09:46:08 +0800

    merged feature authentication main"

--- src/main/java/com/example/demo4/HelloController.java
+++ src/main/java/com/example/demo4/HelloController.java
@@ -1,4 +1,11 @@
 package com.example.demo4;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class HelloController {
+    public Label welcomeText;
+
+    public void onHelloButtonClick(ActionEvent actionEvent) {
+    }
 }
